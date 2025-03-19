ea60f9514ad25e51eebf1a1129fb29effdb743fb
Petter Bortne <nyegolfsko@hotmai.com>
2025-03-19 19:25:57 +0100

    RPLidar

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -30,7 +30,13 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
     
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
